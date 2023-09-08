2f53d0a9746ea2f6b0c8479b46b8f4efa2803e36
wknowpark797 <wknowpark797@gmail.com>
2023-09-08 12:08:43 +0900

    스크롤시 Header 배경색 스타일 변경 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,20 @@ import logo from '@/public/img/logo.svg';
 import profile from '@/public/img/profile.png';
 import { FaBell, FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 function Header() {
+	const [Scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => (window.scrollY > 0 ? setScrolled(true) : setScrolled(false));
+		window.addEventListener('scroll', handleScroll);
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
+		<header className={`${Scrolled && 'bg-[#141414]'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<h1>
 					<Image src={logo} alt='logo' width={100} height={100} className='cursor-pointer' />

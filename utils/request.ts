204c58afe3c2c240ebd7a5fce4f66dcb3bd2f5f7
wknowpark797 <wknowpark797@gmail.com>
2023-09-08 13:05:20 +0900

    API_KEY 환경변수 등록

--- utils/request.ts
+++ utils/request.ts
@@ -1,8 +1,8 @@
-const API_KEY = 'b18e798ff377ef49f1c335283e7c43d6';
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const requests = {
-	sf: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genred=878`,
+	sf: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=878`,
 };
 
 export default requests;

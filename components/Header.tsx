02be3c1fd0401e6ac5cf9e74fe8deb90ccdfeab6
wknowpark797 <wknowpark797@gmail.com>
2023-09-08 11:15:04 +0900

    Header 메뉴 프리셋 스타일 적용

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,11 @@ function Header() {
 			<h1>NEXTFLIX</h1>
 
 			<ul>
-				<li>menu</li>
-				<li>menu</li>
-				<li>menu</li>
+				<li className='header-link'>Home</li>
+				<li className='header-link'>TV Show</li>
+				<li className='header-link'>Movie</li>
+				<li className='header-link'>New & Popular</li>
+				<li className='header-link'>My List</li>
 			</ul>
 		</header>
 	);

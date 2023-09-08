02009b038496535da8c982b8a20b4e78ac0c60a8
wknowpark797 <wknowpark797@gmail.com>
2023-09-08 12:56:05 +0900

    SSR 방식으로 영화 데이터 props 전달

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import requests from '@/utils/request';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ sf }) => {
+	console.log('props: ', sf);
 	return (
 		<div className='relative h-screen bg-gradient-to-b from-[#333] to-[#141414]'>
 			<Head>
@@ -17,3 +19,11 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const sf = await fetch(requests.sf).then((res) => res.json());
+
+	return {
+		props: { sf: sf.results },
+	};
+};

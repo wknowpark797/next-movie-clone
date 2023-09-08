1e64a62970dea45501c22c362f66d15066a6cef0
wknowpark797 <wknowpark797@gmail.com>
2023-09-08 11:02:08 +0900

    기본 메인 페이지 타입 지정 - Header 컴포넌트 생성 및 연결

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import Header from '@/components/Header';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
 	return (
-		<main>
-			<h1>NEXTFLIX</h1>
-		</main>
+		<div>
+			<Head>
+				<title>NEXTFLIX</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Header />
+			<main>
+				<h1>NEXTFLIX</h1>
+			</main>
+		</div>
 	);
-}
+};
+
+export default Home;

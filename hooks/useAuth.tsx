3e77d51e5df7cdda3eb973996f3e7d7c0c8d1f32
wknowpark797 <wknowpark797@gmail.com>
2023-09-13 13:30:23 +0900

    로그인 인증처리 커스텀 훅 생성 및 적용

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -9,15 +9,27 @@ import { useRouter } from 'next/router';
 import { createContext, useContext, useState, useEffect, useMemo } from 'react';
 import { auth } from '@/firebase';
 
+// 전역으로 전달할 정보값
 interface IAuth {
-	user: User | null;
+	User: User | null;
 	signIn: (email: string, password: string) => Promise<void>;
 	signUp: (email: string, password: string) => Promise<void>;
 	logout: () => Promise<void>;
-	error: string | null;
-	loading: boolean;
+	Error: string | null;
+	Loading: boolean;
 }
 
+// 전역 context의 초기값 (IAuth 타입 지정)
+const AuthContext = createContext<IAuth>({
+	User: null,
+	signIn: async () => {},
+	signUp: async () => {},
+	logout: async () => {},
+	Error: null,
+	Loading: false,
+});
+
+// 전역 context를 루트 컴포넌트에 전달하기 위한 wrapping 컴포넌트
 export const AuthProvider = ({ children }) => {
 	const [Loading, setLoading] = useState<boolean>(false);
 	const [User, setUser] = useState<User | null>(null);
@@ -44,4 +56,52 @@ export const AuthProvider = ({ children }) => {
 			setInitialLoading(false);
 		});
 	}, [router]);
+
+	// 회원가입 함수
+	const signUp = async (email: string, password: string) => {
+		setLoading(true);
+		await createUserWithEmailAndPassword(auth, email, password)
+			.then((userInfo) => {
+				setUser(userInfo.user);
+				router.push('/');
+				setLoading(false);
+			})
+			.catch((err) => alert(err.message))
+			.finally(() => setLoading(false));
+	};
+
+	// 로그인 함수
+	const signIn = async (email: string, password: string) => {
+		setLoading(true);
+		await signInWithEmailAndPassword(auth, email, password)
+			.then((userInfo) => {
+				setUser(userInfo.user);
+				router.push('/');
+				setLoading(false);
+			})
+			.catch((err) => alert(err.message))
+			.finally(() => setLoading(false));
+	};
+
+	// 로그아웃 함수
+	const logout = async () => {
+		setLoading(true);
+		signOut(auth)
+			.then(() => {
+				setUser(null);
+			})
+			.catch((err) => alert(err.message))
+			.finally(() => setLoading(false));
+	};
+
+	return (
+		<AuthContext.Provider value={{ User, signIn, signUp, logout, Error, Loading }}>
+			{!InitialLoading && children}
+		</AuthContext.Provider>
+	);
 };
+
+// 전역 context에 접근하기 위한 커스텀 훅
+export default function useAuth() {
+	return useContext(AuthContext);
+}

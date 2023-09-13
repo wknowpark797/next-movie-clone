9660885dce9ee023d8ef1c0f2f6b84d237249592
wknowpark797 <wknowpark797@gmail.com>
2023-09-13 12:08:34 +0900

    인증 성공시 콜백함수로 value값 전달

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,9 +13,16 @@ function Login() {
 	const [Login, setLogin] = useState<boolean>(false);
 	const {
 		register, // 원하는 input요소에 전개연산자로 등록해서 값 관리
-		handleSubmit, // submit 이벤트 발생시 register에 등록된 input값들의 인증처리 함수
+		handleSubmit, // submit 이벤트 발생시 register에 등록된 input값들의 인증처리 함수, 인증완료시 처리할 추가작업의 함수를 콜백으로 등록가능
 		formState: { errors }, // 인증 실패시 커스텀 에러 메세지를 등록할 수 있는 객체
 	} = useForm<Inputs>();
+
+	// handleSubmit 함수가 인증처리 완료시 동기적으로 실행될 콜백함수 등록
+	// 해당 콜백함수는 인증에 성공했을 때 호출: 인수로 전달되는 값은 관리되고 있는 form의 value값
+	const join: SubmitHandler<Inputs> = async ({ email, password }) => {
+		console.log('email: ', email);
+		console.log('password: ', password);
+	};
 
 	return (
 		<main className='relative flex w-full h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
@@ -42,12 +49,28 @@ function Login() {
 				sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 			/>
 
-			<form className='relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+			<form
+				className='relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-md md:px-14'
+				onSubmit={handleSubmit(join)} // submit 이벤트 발생시 handleSubmit이 인증처리를 해주고 인증의 결과값을 등록된 콜백함수에 전달
+			>
 				<h1 className='text-4xl font-semibold'>Sign In</h1>
 
 				<div className='space-y-4'>
-					<input type='email' placeholder='Email' className='input' />
-					<input type='password' placeholder='Password' className='input' />
+					<input
+						type='email'
+						placeholder='Email'
+						className='input'
+						{...register('email', { required: true })}
+					/>
+					{errors.email && <span>Please enter a valid Email.</span>}
+
+					<input
+						type='password'
+						placeholder='Password'
+						className='input'
+						{...register('password', { required: true })}
+					/>
+					{errors.password && <span>Please enter a valid Password.</span>}
 				</div>
 
 				<button className='w-full rounded bg-[#e40914] py-3 font-semibold'>Sign In</button>

cd362613373e1e86d282d70a4333f780f7f5e341
wknowpark797 <wknowpark797@gmail.com>
2023-09-14 12:21:20 +0900

    recoil 설치 및 설정

---
+++ recoil/globalAtom.ts
@@ -0,0 +1,12 @@
+import { atom } from 'recoil'; // 전역 객체 생성
+import { Movie } from '@/types';
+
+export const modalState = atom<boolean>({
+	key: 'modalState',
+	default: false,
+});
+
+export const movieState = atom<Movie | null>({
+	key: 'movieState',
+	default: null,
+});

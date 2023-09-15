b4c4f4b780e6ef8ceabe1f2933f35af9db42cff3
unknown <wknowpark797@gmail.com>
2023-09-15 09:58:26 +0900

    에러 사항 정리 및 이슈해결

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -46,6 +46,25 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 	const [UserInfo, setUserInfo] = useState<User | null>(null);
 	const router = useRouter();
 
+	/*
+		[ 라우터 이동에 대한 에러 이슈 ]
+		라우터 이동시 라우터의 빠른 이동속도 때문에 abort fetch router 에러 발생
+		
+		[ 원인 ]
+		데이터가 fetching 중이거나 컴포넌트의 추가적인 데이터들이 full loaded되지 않은 상태에서 라우터가 변경될 때
+
+		[ 해결방법 ]
+		1. router.push('/', undefined, {shallow: true})
+		-> 라우터 이동시 fetching 기능을 무시하고 라우터 이동만 처리 (실제 컴포넌트 변경시)
+		
+		2. 커스텀 훅을 생성하고 그 안쪽에서 router.onChangeComplete 이벤트를 활용해서 기존 라우터 변경이 끝난 이후에 router.push 호출 (다른 훅이나 핸들러 함수 안쪽에서 호출 불가능)
+
+		현재 상황에서 위의 두가지 해결방법 모두 적용 불가능
+		-> 전체 컴포넌트를 감싸고 있는 root 컴포넌트이기 때문에 unmount가 불가능하기 때문
+
+		[ 해결방법 결론 ]
+		3. useAuth 훅 안에서 유저 정보값이 변경될때마다 setTimeout으로 강제 debouncing 적용
+	*/
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
 			// 인증상태를 감지
@@ -56,7 +75,11 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 			} else {
 				// 전달받은 인증 정보가 없을 경우
 				setUserInfo(null);
-				router.push('/login');
+
+				// 메인 페이지에서 로그인 페이지로 넘어갈 때 0.3초동안 push가 중복실행되지 않도록 debouncing 적용
+				setTimeout(() => {
+					router.push('/login');
+				}, 300);
 			}
 
 			// 한번이라도 인증로직이 실행되면 초기상태(false) 처리
@@ -72,7 +95,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 		await createUserWithEmailAndPassword(auth, email, password)
 			.then((userInfo) => {
 				setUserInfo(userInfo.user);
-				router.push('/');
 			})
 			.catch((err) => alert(err.message));
 	};
@@ -82,7 +104,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 		await signInWithEmailAndPassword(auth, email, password)
 			.then((userInfo) => {
 				setUserInfo(userInfo.user);
-				router.push('/');
 			})
 			.catch((err) => alert(err.message));
 	};
--- recoil/globalAtom.ts
+++ recoil/globalAtom.ts
@@ -3,11 +3,19 @@ import { atom } from 'recoil'; // 전역 객체 생성
 import { Movie } from '@/types';
 
 export const modalState = atom<boolean>({
-	key: 'modalState',
+	/*
+		[ recoil key값에 대한 에러 이슈 ]
+		atom 객체의 key값이 고유값으로 인지되어야 하지만 next에서는 pre-render가 미리 실행되기 때문에
+		서버에서 한번, hydrate때 한번 실행 (Next에서만 발생하는 문제점)
+		
+		[ 해결방법 ]
+		컴포넌트가 구동될 때마다 고유값을 추가로 뒤에 붙여 key값 중복오류를 해결
+	*/
+	key: `modalState${performance.now()}`,
 	default: false,
 });
 
 export const movieState = atom<Movie | null>({
-	key: 'movieState',
+	key: `movieState${performance.now()}`,
 	default: null,
 });
